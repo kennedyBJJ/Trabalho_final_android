f746939bcfc2d792edfe0628044e4ce548aca173
Bruna Cristina <brunacristinabrandao43@gmail.com>
2023-11-09 15:39:58 -0300

    layout Tela Splash

--- app/src/main/java/com/example/trabalhofinal/MainActivity.java
+++ app/src/main/java/com/example/trabalhofinal/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.trabalhofinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Timer
+    private static int SPLASH_TIME_OUT = 4000;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, Login.class);
+                startActivity(i);
+
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
     }
 }
