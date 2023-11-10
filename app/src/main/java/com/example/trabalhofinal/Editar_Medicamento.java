e9a0435055adc628c27c91682e65507c6d79f80c
Bruna Cristina <brunacristinabrandao43@gmail.com>
2023-11-10 17:20:50 -0300

    Todos os layouts criados.

--- app/src/main/java/com/example/trabalhofinal/Editar_Medicamento.java
+++ app/src/main/java/com/example/trabalhofinal/Editar_Medicamento.java
@@ -2,18 +2,13 @@ package com.example.trabalhofinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-public class Inicio extends AppCompatActivity {
-
+public class Editar_Medicamento extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_inicio);
-
-
+        setContentView(R.layout.activity_editar_medicamento);
     }
 }
--- app/src/main/java/com/example/trabalhofinal/Login.java
+++ app/src/main/java/com/example/trabalhofinal/Login.java
@@ -6,35 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import java.text.Normalizer;
-
 public class Login extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        androidx.appcompat.widget.AppCompatButton buttonEntrar = findViewById(R.id.btnEntrar);
-        androidx.appcompat.widget.AppCompatButton buttonCriarConta = findViewById(R.id.btnCriarConta);
-        buttonEntrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Criar uma intenção para abrir Inicio
-                Intent intent = new Intent(Login.this,Inicio.class);
-                startActivity(intent);
 
-            }
-        });
-
-        buttonCriarConta.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Criar uma intenção para abrir Form_Cadastro
-                Intent intent = new Intent(Login.this,Form_Cadastro.class);
-                startActivity(intent);
-            }
-        });
 
     }
 }
