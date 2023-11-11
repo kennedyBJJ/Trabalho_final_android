189c2171ac81a62877af7eab09112a093d514584
MarcosBritoSAR <118199131+cintiateixeira@users.noreply.github.com>
2023-11-11 13:03:03 -0300

    iniciando Geolocalizacao

---
+++ app/src/main/java/com/example/trabalhofinal/localization/localizacaoMain.java
@@ -0,0 +1,16 @@
+package com.example.trabalhofinal.localization;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.trabalhofinal.R;
+
+public class localizacaoMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_localizacao_main);
+    }
+}
