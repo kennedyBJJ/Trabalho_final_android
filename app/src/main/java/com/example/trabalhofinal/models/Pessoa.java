544a9ddf814bff3de3d6f899f3033edb23672b6c
kennedyBJJ <116119627+kennedyBJJ@users.noreply.github.com>
2023-11-13 17:41:58 -0300

    Criado modelo de pessoa

---
+++ app/src/main/java/com/example/trabalhofinal/models/Pessoa.java
@@ -0,0 +1,43 @@
+package com.example.trabalhofinal.models;
+
+import java.io.Serializable;
+
+public class Pessoa implements Serializable {
+
+    private String NOME_USER;
+    private String DATA_NASCIMENTO;
+    private String TELEFONE;
+    private String SENHA;
+
+    public String getNOME_USER() {
+        return NOME_USER;
+    }
+
+    public void setNOME_USER(String NOME_USER) {
+        this.NOME_USER = NOME_USER;
+    }
+
+    public String getDATA_NASCIMENTO() {
+        return DATA_NASCIMENTO;
+    }
+
+    public void setDATA_NASCIMENTO(String DATA_NASCIMENTO) {
+        this.DATA_NASCIMENTO = DATA_NASCIMENTO;
+    }
+
+    public String getTELEFONE() {
+        return TELEFONE;
+    }
+
+    public void setTELEFONE(String TELEFONE) {
+        this.TELEFONE = TELEFONE;
+    }
+
+    public String getSENHA() {
+        return SENHA;
+    }
+
+    public void setSENHA(String SENHA) {
+        this.SENHA = SENHA;
+    }
+}
