01cc7802fc01e52a8b44033f36fcb10b581a2321
MarcosBritoSAR <marcosbryto92@gmail.com>
2023-11-14 01:39:07 -0300

    ajustando botão da localizacao

--- app/src/main/java/com/example/trabalhofinal/Meus_Lembretes.java
+++ app/src/main/java/com/example/trabalhofinal/Meus_Lembretes.java
@@ -2,13 +2,27 @@ package com.example.trabalhofinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Meus_Lembretes extends AppCompatActivity {
+
+    private Button map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meus_lembretes);
+        map = findViewById(R.id.btnFarmacia);
+
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             new Intent(Meus_Lembretes.this, com.example.trabalhofinal.Localization.LocalizacaoMain.class);
+            }
+        });
     }
+
 }
